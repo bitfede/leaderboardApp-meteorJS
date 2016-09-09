df07c4334fc7ddc04ff4282e1dcab39c7fb080f4
Fede Defaveri <f.g.defaveri@gmail.com>
2016-09-09 12:25:11 -0400

    learned the new and correct way to create helper functions

--- leaderboard.js
+++ leaderboard.js
@@ -3,8 +3,14 @@ console.log('Hello world!');
 PlayersList = new Mongo.Collection('players');
 
 if (Meteor.isClient) {
-	Template.leaderboard.player = function() {
-		return 'some text';
+	Template.leaderboard.helpers = function() {
+		//helper functions go here in JSON format
+		'player': function() {
+			return "Some text";
+		},
+		'player2': function() {
+			return "Some other text";
+		}
 	};
 }
 
