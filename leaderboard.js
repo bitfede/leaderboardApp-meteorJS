b8340840bb8f7b43b7c4b15268850f17d22cae7d
Fede Defaveri <f.g.defaveri@gmail.com>
2016-09-09 12:13:44 -0400

    learned about client-only and server-only execution plus added a template to main view

--- leaderboard.js
+++ leaderboard.js
@@ -1,3 +1,11 @@
 console.log('Hello world!');
 
 PlayersList = new Mongo.Collection('players');
+
+if (Meteor.isClient) {
+	console.log('Hello Client!');
+}
+
+if (Meteor.isServer) {
+	console.log('Hello Server');
+}
