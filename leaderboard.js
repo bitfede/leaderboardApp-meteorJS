9bb2d808fb88e3ae7534ad21b52a740900a4bbd7
Fede Defaveri <f.g.defaveri@gmail.com>
2016-09-12 10:45:14 -0400

    created the selected effect using the meteor session official package

--- leaderboard.js
+++ leaderboard.js
@@ -7,12 +7,20 @@ if (Meteor.isClient) {
 		//helper functions go here in JSON format
 		'player' : function() {
 			return PlayersList.find();
+		},
+		'selectedClass': function() {
+			var playerId = this._id;
+			var selectedPlayer = Session.get('selectedPlayer');
+			if (selectedPlayer == playerId) {
+				return "selected";
+			}
 		}
 	});
 
 	Template.leaderboard.events({
 		'click .player': function() {
-			console.log('You clicked a player element!')
+			var playerID = this._id;
+			Session.set('selectedPlayer', playerID);
 		}
 	});
 }
