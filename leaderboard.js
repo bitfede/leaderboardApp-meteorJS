ba90a79310b24b2303ce9aa5cac3ec3f0654b17e
Fede Defaveri <f.g.defaveri@gmail.com>
2016-09-13 18:12:11 -0400

    added a form to add new players, now implementing a button to remove players

--- leaderboard.js
+++ leaderboard.js
@@ -3,6 +3,9 @@ console.log('Hello world!');
 PlayersList = new Mongo.Collection('players');
 
 if (Meteor.isClient) {
+
+	//---------LEADERBOARD template
+
 	Template.leaderboard.helpers( {
 		//helper functions go here in JSON format
 		'player' : function() {
@@ -33,10 +36,46 @@ if (Meteor.isClient) {
 		'click .decrement': function() {
 			var playerID = Session.get('selectedPlayer');
 			PlayersList.update({ _id: playerID }, {$inc: {score: -5}});
+		},
+		'click .remove': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    console.log(selectedPlayer + "ssss")
 		}
+	});
+
+	//----------ADDPLAYERFORM template
+
+	Template.addPlayerForm.events({
+		'submit form': function(event) {
+			event.preventDefault();
+			var playerNameVar = event.target.playerName.value;
+			PlayersList.insert({
+				name: playerNameVar,
+				score: 0
+			});
+			event.target.playerName.value = "";
+			}
+
 	});
 }
 
 if (Meteor.isServer) {
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
